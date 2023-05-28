05b3141f79deefe7323b95f5f3b884f25613b2a2
Ubuntu <ubuntu@ip-172-31-28-146.us-west-1.compute.internal>
2023-05-28 04:17:29 +0000

    change mongo.js

--- models/mongo.js
+++ models/mongo.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb://127.0.0.1:27017/restaurants', {useNewURLParser: true})
+mongoose.connect('mongodb://127.0.0.1:27017/random', {useNewURLParser: true})
         .then(()=> {
             console.log('数据库连接成功');
         })
